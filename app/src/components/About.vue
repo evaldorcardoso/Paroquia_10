9c6c8f9e43985a4ef9d62e8e86910d9d61561c69
Evaldo R. C <evaldorubel@hotmail.com>
2021-12-03 19:37:00 -0300

    Pagina sobre

---
+++ app/src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="text-center">
+        
+        <img class="text-center mt-5" src="https://www.paroquia10.com/images/icone_transp_compl.png" alt="" height="200">
+        <p class="text-center mt-300">Desenvolvido e mantido por:</p>
+        <a href="https://evaldorc.com.br"><img src="https://evaldorc.com.br/assets/images/marca_b.png"/></a>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    p{
+        color: gray;
+    }
+    .mt-300{
+        margin-top: 300px;
+    }
+</style>
